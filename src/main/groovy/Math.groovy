05c43886e61b342552973ef37bf5fb185c477cc9
Elgars Upitis <elgars.upitis@gmail.com>
2023-03-13 20:43:56 +0200

    Update sandbox files

---
+++ src/main/groovy/Math.groovy
@@ -0,0 +1,17 @@
+class Math {
+    static void main(String[] args) {
+
+        println("5 + 4 = " + (5 + 4))
+        println("5 - 4 = " + (5 - 4))
+        println("5 * 4 = " + (5 * 4))
+        println("5 / 4 = " + (5 / 4))
+        println("5 % 4 = " + (5 % 4))
+
+        println("5.2 + 4.4 = " + (5.2 + 4.4))
+        println("5.2 - 4.4 = " + (5.2 - 4.4))
+        println("5.2 * 4.4 = " + (5.2 * 4.4))
+        println("5.2 / 4.4 = " + (5.2 / 4.4))
+
+
+    }
+}
